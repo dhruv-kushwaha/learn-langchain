67ada0457cb0108125c424e0afa70b6422b03d46
Dhruv Kushwaha <dhruv.kushwaha@optimusinfo.com>
2026-01-05 15:01:22 +0530

    Creating dynamic prompts using PromptTemplate

--- 02_prompts/src/prompt_ui.py
+++ 02_prompts/src/prompt_ui.py
@@ -1,6 +1,8 @@
 from langchain_openai import AzureChatOpenAI
 from dotenv import load_dotenv
+import streamlit as st
 import os
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 
@@ -11,9 +13,41 @@ model = AzureChatOpenAI(
     azure_deployment=deployment_name,
     api_version=api_version,
     temperature=0.7,
-    max_tokens=100,  # type: ignore
+    max_tokens=1000,  # type: ignore
 )
 
-response = model.invoke("Explain the theory of relativity in simple terms.")
+st.header("Research Tool")
 
-print(response.content)
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = PromptTemplate(
+    template="""
+        Please summarize the research paper titled "{paper_input}" with the following specifications:
+        Explanation Style: {style_input}  
+        Explanation Length: {length_input}  
+        1. Mathematical Details:  
+        - Include relevant mathematical equations if present in the paper.  
+        - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
+        2. Analogies:  
+        - Use relatable analogies to simplify complex ideas.  
+        If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+        Ensure the summary is clear, accurate, and aligned with the provided style and length.
+        """,
+    input_variables=['paper_input', 'style_input','length_input'],
+    # validate_template=True
+)
+
+prompt = template.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+
+
+if st.button('Summarize'):
+    result = model.invoke(prompt)
+    st.write(result.content)
