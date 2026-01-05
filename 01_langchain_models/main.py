a2532107dd576d3c4888b2f713b69dd34b7b0f46
Dhruv Kushwaha <dhruv.kushwaha@optimusinfo.com>
2026-01-05 12:41:53 +0530

    demo ChatHuggingFace using HuggingFace inference API

--- 01_langchain_models/main.py
+++ 01_langchain_models/main.py
@@ -1,11 +1,16 @@
-from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-model_name = os.getenv("HUGGINGFACE_MODEL")
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",  # Use a chat-capable model
+    task="text-generation"
+) # type: ignore
 
-print(api_token)
-print(model_name)
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke("What is the capital of France?")
+
+print(response.content)
